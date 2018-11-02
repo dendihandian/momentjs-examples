759f496f835222dc6efce8d4104e8d60ff3e2ff8
Dendi Handian <handian.dendi@gmail.com>
2018-11-03 00:11:25 +0700

    update parse test

--- tests/parse.js
+++ tests/parse.js
@@ -2,7 +2,25 @@ const moment = require('moment')
 const assert = require('assert')
 
 describe('Parse', function() {
-  it('YYYY-MM-DD', function() {
-    assert.equal(moment('1994-08-17').isValid(), true)
+  describe('Now', function() {
+    it('Empty Parameter', function() {
+      assert.equal(moment().isValid(), true)
+    })
+    it('Empty Array Parameter', function() {
+      assert.equal(moment([]).isValid(), true)
+    })
+    it('Empty Object Parameter', function() {
+      assert.equal(moment({}).isValid(), true)
+    })
+  })
+  describe('String', function() {
+    describe('YYYY-MM-DD', function() {
+      const indonesiaIndependenceDay = '1994-08-17'
+      describe(indonesiaIndependenceDay, function(indonesiaIndependenceDay) {
+        it ('.isValid()', function() {
+          assert.equal(moment(indonesiaIndependenceDay).isValid(), true)
+        })
+      })
+    })
   })
 })
